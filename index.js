8fa57cd83ce9daaab345f21597cd180b6f404ff1
Romklao <rk120879@gmail.com>
2018-06-14 15:59:13 -0700

    feat: Create the function that allow show and hide modal

--- index.js
+++ index.js
@@ -7,19 +7,36 @@ const main = document.querySelector('main');
 const drawer = document.querySelector('.nav-menu');
 const footer = document.querySelector('.footer-social');
 const contact = document.querySelector('#contact');
+const about = document.querySelector('#about');
+const aboutText = document.querySelector('.about-text');
+const overlay = document.querySelector('#overlay');
+const closeModal = document.querySelector('.close-modal');
 
 menu.addEventListener('click', function(e) {
-  drawer.classList.toggle('open');
   e.stopPropagation();
+  drawer.classList.toggle('open');
 });
 
-main.addEventListener('click', function() {
+main.addEventListener('click', function(e) {
+  e.stopPropagation();
   drawer.classList.remove('open');
 });
 
 contact.addEventListener('click', function(e) {
-  footer.scrollIntoView();
   e.stopPropagation();
+  footer.scrollIntoView();
+});
+
+about.addEventListener('click', function(e) {
+  e.stopPropagation();
+  aboutText.className = 'show';
+  overlay.className = 'profile-overlay';
+});
+
+closeModal.addEventListener('click', function(e) {
+  e.stopPropagation();
+  overlay.className = 'hide';
+  aboutText.className = 'hide';
 })
 
 
