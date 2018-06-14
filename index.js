7eeaadca04cf1c4bca844080f9955be514163605
Romklao <rk120879@gmail.com>
2018-06-13 22:34:21 -0700

    feat: Create the function to jump to the element when clicked

--- index.js
+++ index.js
@@ -2,16 +2,29 @@
 
 // TODO: click to open a menu bar for a small screen
 
-var menu = document.querySelector('#menu');
-var main = document.querySelector('main');
-var drawer = document.querySelector('.nav-menu');
+const menu = document.querySelector('#menu');
+const main = document.querySelector('main');
+const drawer = document.querySelector('.nav-menu');
+const footer = document.querySelector('.footer-social');
+const contact = document.querySelector('#contact');
 
 menu.addEventListener('click', function(e) {
   drawer.classList.toggle('open');
   e.stopPropagation();
 });
+
 main.addEventListener('click', function() {
   drawer.classList.remove('open');
 });
+
+contact.addEventListener('click', function(e) {
+  footer.scrollIntoView();
+  e.stopPropagation();
+})
+
+
+
+
+
 
 
